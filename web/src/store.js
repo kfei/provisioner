a8e45221e12f18407de9f8754cbdd1d5140535de
kfei <kfei@kfei.net>
2018-10-23 11:00:01 +0900

    web: add transition effect to available coupons
    Signed-off-by: kfei <kfei@kfei.net>

--- web/src/store.js
+++ web/src/store.js
@@ -6,11 +6,13 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     available: 0,
-    wsStatus: 0
+    wsStatus: 0,
+    delta: 0
   },
 
   mutations: {
     SetAvailable (state, { count = 0 }) {
+      state.delta = count - state.available
       state.available = count
     },
 
--- web/src/views/Coupon.vue
+++ web/src/views/Coupon.vue
@@ -4,7 +4,7 @@
       <v-flex xs12>
         <div class="available d-flex" style="flex-direction: column;">
           <div class="description headline mt-5 grey--text">Coupons Available:</div>
-          <div class="coupon primary--text">{{ available }}</div>
+          <div class="coupon primary--text" :class="{ increased: increased, decreased: decreased }">{{ available }}</div>
         </div>
       </v-flex>
       <v-flex xs12>
@@ -23,7 +23,9 @@ import ws from '@/ws'
 export default {
   computed: {
     ...mapState({
-      'available': state => state.available
+      'available': state => state.available,
+      'increased': state => state.delta > 0,
+      'decreased': state => state.delta < 0
     })
   },
 
@@ -44,4 +46,25 @@ export default {
 .available
   .coupon
     font-size 200px
+
+    &.increased
+      animation color-increased 1s infinite
+    &.decreased
+      animation color-decreased 1s infinite
+
+primaryColor = '#3498db'
+@keyframes color-increased
+  0%
+    color primaryColor
+  50%
+    color green
+  100%
+    color primaryColor
+@keyframes color-decreased
+  0%
+    color primaryColor
+  50%
+    color red
+  100%
+    color primaryColor
 </style>
